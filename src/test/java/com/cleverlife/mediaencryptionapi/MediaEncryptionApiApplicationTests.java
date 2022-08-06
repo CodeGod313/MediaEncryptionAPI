fb4712d50c460a8fc3feef48c51cbe3351c567fc
Codegod <codegod313@gmail.com>
2022-08-06 14:28:40 +0300

    Initial commit

---
+++ src/test/java/com/cleverlife/mediaencryptionapi/MediaEncryptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleverlife.mediaencryptionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaEncryptionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
